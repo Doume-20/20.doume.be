5bd86fcf053f3e743bdfe2f08b4610f8fca6435d
Doume20 <153982012+Doume-20@users.noreply.github.com>
2026-09-06 13:15:29 +0200

    added a test meta

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import HeroSection from "@/components/sections/HeroSection.vue"
+
+definePageMeta({
+  title: 'Doume\'s portfolio',
+  meta: [
+    { name: 'description', content: '\'jour' },
+    { property: 'og:title', content: '\'jour' }
+  ]
+})
 </script>
 
 <template>
